fc092c587a36a758bdbf1923ab133baa04958392
Anton <iamanton45@gmail.com>
2021-10-29 19:21:23 +0300

    FB16

--- WebApplication.Services/CurrencyService.cs
+++ WebApplication.Services/CurrencyService.cs
@@ -84,84 +84,37 @@ namespace WebApplication.Services
             var currencyHistory = await _currencyRepository.GetHistory(currencyId, scale, dtStart, dtFinal);
             List<CurrencyHistoryDto> dataCH = null;
             int minLengthData = 1;
-            double differenceBetweenTrend;
 
             if (currencyHistory.Count > minLengthData)
             {
                 dataCH = currencyHistory.ToList();
 
                 //временное решение
-                foreach (var item in dataCH)
+                for (int i = 0; i < dataCH.Count; i++)
                 {
-                    item.Buy = item.Sale;
+                    dataCH[i].Buy = dataCH[i].Sale;
+                    dataCH[i].Data = new DateTime(2000 + i, 7, 20);
                 }
 
-                List<Step> storySteps = FindStoryStepData(dataCH);
+                (double max, double min) globalAverage = DefineAverage(dataCH);
 
-                (double max, double min) globalAverage = DefineAverage(storySteps);
+                List<int> addressTrends = DefineTrend(dataCH);
 
-                Stack<int> addressTrends = DefinesTrend(dataCH, globalAverage);
-
-                //for (int t = 0; t < addressTrends.Count - 1; t++)
-                while(addressTrends.Count > 0)
-                {
-                    var con = addressTrends.Pop();
-                    var neCon = addressTrends.Peek();
-                    if (addressTrends.Count == 1)
-                    {
-                        neCon = addressTrends.Pop();
-                    }
-
-                    differenceBetweenTrend = (dataCH[con].Buy - dataCH[neCon].Buy) / (con - neCon);
-                    for (int i = neCon + 1; i < con; i++)
-                    {
-                        dataCH[i].Buy = dataCH[i - 1].Buy + differenceBetweenTrend;
-                    }
-                }
+                dataCH = EditingGraph(dataCH, addressTrends);
 
             }
 
             return dataCH;
         }
 
-        private class Step
-        {
-            public Step(double trend, int max, int min)
-            {
-                Trend = trend;
-                Max = max;
-                Min = min;
-            }
-
-            public double Trend { get; }
-            public int Max { get; }
-            public int Min { get; }
-        }
-
-        private List<Step> FindStoryStepData(List<CurrencyHistoryDto> dataCH)
-        {
-            List<Step> storyStep = new List<Step>();
-
-            for (int i = 0; i < dataCH.Count - 1; i++)
-            {
-                storyStep.Add(
-                    new Step(dataCH[i + 1].Buy - dataCH[i].Buy
-                    , dataCH[i].Buy > dataCH[i + 1].Buy ? i : i + 1
-                    , dataCH[i].Buy < dataCH[i + 1].Buy ? i : i + 1
-                    ));
-            }
-
-            return storyStep;
-        }
-
-        private (double max, double min) DefineAverage(List<Step> storySteps)
+        private (double max, double min) DefineAverage(List<CurrencyHistoryDto> dataCH)
         {
             int max = 0;
             int min = 0;
             double maxHistory = 0;
             double minHistory = 0;
 
-            if (storySteps[0].Trend > 0)
+            if (dataCH[1].Buy - dataCH[0].Buy > 0)
             {
                 max++;
             }
@@ -170,45 +123,96 @@ namespace WebApplication.Services
                 min++;
             }
 
-            for (int t = 0; t < storySteps.Count - 1; t++)
+            for (int t = 1; t < dataCH.Count - 1; t++)
             {
-                if (storySteps[t].Trend > 0)
+                if (dataCH[t - 1].Buy - dataCH[t].Buy > 0)
                 {
-                    maxHistory += storySteps[t].Trend;
+                    maxHistory += dataCH[t - 1].Buy - dataCH[t].Buy;
 
-                    if (storySteps[t + 1].Trend < 0)
+                    if (dataCH[t].Buy - dataCH[t+1].Buy < 0)
                     {
                         min++;
                     }
                 }
                 else
                 {
-                    minHistory += storySteps[t].Trend;
+                    minHistory += dataCH[t - 1].Buy - dataCH[t].Buy;
 
-                    if (storySteps[t + 1].Trend > 0)
+                    if (dataCH[t].Buy - dataCH[t+1].Buy > 0)
                     {
                         max++;
                     }
                 }
             }
 
-            if (storySteps[storySteps.Count - 1].Trend < 0)
+            if (dataCH[dataCH.Count - 1].Buy - dataCH[dataCH.Count - 2].Buy < 0)
             {
-                minHistory += storySteps[storySteps.Count - 1].Trend;
+                minHistory += dataCH[dataCH.Count - 1].Buy - dataCH[dataCH.Count - 2].Buy;
             }
             else
             {
-                maxHistory += storySteps[storySteps.Count - 1].Trend;
+                maxHistory += dataCH[dataCH.Count - 1].Buy - dataCH[dataCH.Count - 2].Buy;
             }
-
-            //if(Math.Abs(minHistory) / maxHistory > maxHistory / Math.Abs(minHistory)
-            //    > 3)
 
             maxHistory = maxHistory / max;
             minHistory = minHistory / min;
-
             return (maxHistory, minHistory);
         }
+
+        private List<int> DefineTrend(List<CurrencyHistoryDto> dataCH)
+        {
+            List<int> addressTrends = new List<int> {0};
+
+            for (int t = 1; t < dataCH.Count - 1; t++)
+            {
+                if ((dataCH[t].Buy - dataCH[t + 1].Buy < 0
+                    && dataCH[t - 1].Buy - dataCH[t].Buy > 0)
+                    || (dataCH[t].Buy - dataCH[t + 1].Buy > 0
+                    && dataCH[t - 1].Buy - dataCH[t].Buy < 0))
+                {
+                    addressTrends.Add(t);
+                }                
+            }
+
+            addressTrends.Add(dataCH.Count - 1);
+
+            return addressTrends;
+        }
+
+        private List<CurrencyHistoryDto> EditingGraph(List<CurrencyHistoryDto> dataCH, List<int> addressTrends)
+        {
+            double differenceBetweenTrend;
+
+            for (int t = 0; t < addressTrends.Count - 1; t++)
+            {
+                differenceBetweenTrend = (dataCH[addressTrends[t + 1]].Buy - dataCH[addressTrends[t]].Buy) / (addressTrends[t + 1] - addressTrends[t]);
+                for (int i = addressTrends[t] + 1; i < addressTrends[t + 1]; i++)
+                {
+                    dataCH[i].Buy = dataCH[i - 1].Buy + differenceBetweenTrend;
+                }
+            }
+            //while (addressTrends.Count > 0)
+            //{
+            //    var con = addressTrends.Pop();
+            //    var neCon = addressTrends.Peek();
+            //    if (addressTrends.Count == 1)
+            //    {
+            //        neCon = addressTrends.Pop();
+            //    }
+
+                
+            //}
+
+            return dataCH;
+        }
+
+        private List<CurrencyHistoryDto> OptimizingGraphics(List<CurrencyHistoryDto> dataCH, (double max, double min) globalAverage)
+        {
+
+
+            return dataCH;
+        }
+
 
         private Stack<int> DefinesTrend(List<CurrencyHistoryDto> dataCH, (double max, double min) globalAverage)
         {
@@ -218,33 +222,37 @@ namespace WebApplication.Services
 
             for (int t = 1; t < dataCH.Count - 1; t++)
             {
-                if (t == 14)
+
+                if (t == 58)
                 {
 
                 }
 
                 longTrend++;
 
-                //if (address.Count > 1)
-                //{
-                //    var fg = address.Pop();
-                //    if ((dataCH[address.Peek()].Buy - dataCH[fg].Buy > 0
-                //        && dataCH[address.Peek()].Buy - dataCH[t - longTrend].Buy > 0)
-                //    || (dataCH[address.Peek()].Buy - dataCH[fg].Buy < 0
-                //        && dataCH[address.Peek()].Buy - dataCH[t - longTrend].Buy < 0))
-                //    {
+                if (address.Count > 1)
+                {
+                    var fg = address.Pop();
+                    if (((dataCH[address.Peek()].Buy - dataCH[fg].Buy > 0
+                        && dataCH[address.Peek()].Buy - dataCH[t].Buy > 0)
+                    || (dataCH[address.Peek()].Buy - dataCH[fg].Buy < 0
+                        && dataCH[address.Peek()].Buy - dataCH[t].Buy < 0))
+                    && Math.Abs(dataCH[address.Peek()].Buy - dataCH[fg].Buy) < Math.Abs(dataCH[address.Peek()].Buy - dataCH[t].Buy))
+                    {
+                        longTrend = 0;
+                    }
+                    else
+                    {
+                        address.Push(fg);
+                    }
 
-                //    }
-                //    else
-                //    {
-                //        address.Push(fg);
-                //    }
-                //}
 
-                //if (Math.Abs(dataCH[t].Buy - dataCH[address.Peek()].Buy) > Math.Abs(dataCH[address.Peek()].Buy - dataCH[t - longTrend].Buy))
-                //{
-                //    longTrend = t - address.Peek();
-                //}
+                }
+
+                if (Math.Abs(dataCH[t].Buy - dataCH[address.Peek()].Buy) > Math.Abs(dataCH[address.Peek()].Buy - dataCH[t - longTrend].Buy))
+                {
+                    longTrend = t - address.Peek();
+                }
 
                 if ((dataCH[t + 1].Buy - dataCH[t].Buy < 0 && 
                     dataCH[t].Buy - dataCH[t - longTrend].Buy > globalAverage.max)
@@ -255,6 +263,7 @@ namespace WebApplication.Services
                     {
                         address.Push(t - longTrend);
                     }
+
                     longTrend = 0;
                 }
             }
@@ -269,7 +278,10 @@ namespace WebApplication.Services
 
             return address;
         }
-        
+
+
+
+
         private double FindTrend(List<CurrencyHistoryDto> dataCH, int start, int finish)//  bool flagPositiveTrend = true)
         {
             double summ = 0;
@@ -283,51 +295,41 @@ namespace WebApplication.Services
         }
 
 
-
-        private double DefineTrend(List<CurrencyHistoryDto> dataCH, int start, int finish)
-        {
-            int localMiddle = finish - start;
-            //int step = 0;
-            //double localMiddleDegree = 0;
-            double summ = 0;
-
-            if (localMiddle > 0)
-            {
-                //for (int t = 1; t <= localMiddle; t++)
-                //{
-                //    step = step + t;
-                //}
-
-                //localMiddleDegree = 1.0 / step;
-                //step = 0;
-
-                //for (int t = start; t < finish; t++)
-                //{
-                //    step++;
-                //    summ += (dataCH[t + 1].Buy - dataCH[t].Buy) * (localMiddleDegree * step);
-                //}
-
-
-                for (int t = start; t < finish; t++)
-                {
-                    summ += dataCH[t + 1].Buy - dataCH[t].Buy;
-                }
-
-            }
-
-            return summ;
-        }
-
-        private int TrendMin(List<CurrencyHistoryDto> dataCH, int position, int minDataAddress)
-        {
-            return dataCH[position].Buy < dataCH[minDataAddress].Buy ? position : minDataAddress;
-        }
-
         private int TrendMax(List<CurrencyHistoryDto> dataCH, int position, int maxDataAddress)
         {
             return dataCH[position].Buy > dataCH[maxDataAddress].Buy ? position : maxDataAddress;
         }
 
+
+        //private class Step
+        //{
+        //    public Step(double trend, int max, int min)
+        //    {
+        //        Trend = trend;
+        //        Max = max;
+        //        Min = min;
+        //    }
+
+        //    public double Trend { get; }
+        //    public int Max { get; }
+        //    public int Min { get; }
+        //}
+
+        //private List<Step> FindStoryStepData(List<CurrencyHistoryDto> dataCH)
+        //{
+        //    List<Step> storyStep = new List<Step>();
+
+        //    for (int i = 0; i < dataCH.Count - 1; i++)
+        //    {
+        //        storyStep.Add(
+        //            new Step(dataCH[i + 1].Buy - dataCH[i].Buy
+        //            , dataCH[i].Buy > dataCH[i + 1].Buy ? i : i + 1
+        //            , dataCH[i].Buy < dataCH[i + 1].Buy ? i : i + 1
+        //            ));
+        //    }
+
+        //    return storyStep;
+        //}
 
     }
 }
